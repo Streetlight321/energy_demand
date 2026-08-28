b88a73bb6a62a0a1e79f861ada9edb360184395d
Yousef Eddin <yousef.eddin123@gmail.com>
2026-08-28 11:13:01 -0400

    Seperating the extract with the load.

---
+++ transform/to_bronze.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+def to_bronze_pipe(df):
+    df = df.copy()
+
+    df = df.rename(
+        columns={
+            "respondent-name": "respondent_name",
+            "type-name": "type_name",
+            "value-units": "value_units",
+        }
+    )
+
+    df["period"] = (
+        pd.to_datetime(df["period"], utc=True)
+        .dt.strftime("%Y-%m-%dT%H:%M:%SZ")
+    )
+
+    df["value"] = pd.to_numeric(
+        df["value"],
+        errors="coerce"
+    )
+
+    # Convert NaN/NA to None so it becomes SQL NULL
+    df = df.astype(object).where(
+        pd.notna(df),
+        None
+    )
